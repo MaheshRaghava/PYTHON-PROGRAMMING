2862dd9cde332747ce23a6625e7ce3a0ff5d5412
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2024-09-24 12:23:52 +0530

    Update Week-9.py

--- Week-9.py
+++ Week-9.py
@@ -1,5 +1,6 @@
+#Files
 f = open('mahesh.txt','w')
-f.write("Hello cutie")
+f.write("Hello Cutie")
 f = open('mahesh.txt','a')
 f.write(" cse-c")
 f = open('mahesh.txt','r')
@@ -11,3 +12,17 @@ f.read(5)
 pos = f.tell()
 print(pos)
 f.close()
+#Binary file
+f = open('mahesh.txt','wb')
+f.write(b'\x00\x01\x03')
+f = open('mahesh.bin','rb')
+print(f.read())
+f.close()
+#CSV
+import csv
+data=[['s.No','RollNo','Name'],
+     [1,24,'Cutie'],
+     [2,28,'chinnu']]
+file=open('table.csv','w',newline='')
+w=csv.writer(file)
+w.writerows(data)
